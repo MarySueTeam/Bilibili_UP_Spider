c521cb797babdf7582d4414905ce4ce835fdf023
Liona <lionacc@163.com>
2022-04-18 16:20:16 +0800

    format file

--- utils.py
+++ utils.py
@@ -6,31 +6,29 @@ import sqlite3
 import sys
 from you_get import common as you_get
 
-
 hander = RotatingFileHandler("./logs/run.log",
                              encoding="UTF-8",
                              maxBytes=1024 * 1024 * 10,
                              backupCount=10)
 
-
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
     datefmt="%m-%d %H:%M",
-    handlers=[RichHandler(rich_tracebacks=True,markup=True),hander]
-)
+    handlers=[RichHandler(rich_tracebacks=True, markup=True), hander])
 
 
 class Bili_UP:
+
     def __init__(self, mid: str):
 
         self.mid = mid
         self.api_user_info = f'http://api.bilibili.com/x/space/acc/info?mid={mid}'
-        self.api_video_list = f'http://api.bilibili.com/x/space/arc/search?mid={mid}&ps=30&pn=' # page number
+        self.api_video_list = f'http://api.bilibili.com/x/space/arc/search?mid={mid}&ps=30&pn='  # page number
         self.video_url = "https://www.bilibili.com/video/{0}"
         self.user_name = self.get_user_info()['data']['name']
 
-        self.log = logging.getLogger("Bili_UP "+ self.user_name)
+        self.log = logging.getLogger("Bili_UP " + self.user_name)
         self.log.info("[bold green]LOG LOAD SUCCESSFULLY[/]")
         self.log.info(f"[bold blue blink]RUN FOR {self.user_name}[/]")
         self.video_list = []
@@ -46,23 +44,27 @@ class Bili_UP:
         response = requests.get(self.api_video_list)
         self.log.debug(response.json())
         count = response.json()['data']['page']['count']
-        for i in range(count//30+1):
-            res = requests.get(self.api_video_list+str(i+1))
+        for i in range(count // 30 + 1):
+            res = requests.get(self.api_video_list + str(i + 1))
             self.log.debug(res.json())
             self.video_list += res.json()['data']['list']['vlist']
 
-        self.log.info(f"[bold green]GET {len(self.video_list)} VIDEOS FOR {self.user_name}[/]")
+        self.log.info(
+            f"[bold green]GET {len(self.video_list)} VIDEOS FOR {self.user_name}[/]"
+        )
         self.insert_db()
 
     def insert_db(self):
         cursor = self.db.cursor()
         for item in self.video_list:
-            sql = """insert into video_info(aid,bvid,title,author,description,cover_img,is_download) values ('%s','%s','%s','%s','%s','%s','%s');""" % (item['aid'], item['bvid'], item['title'], item['author'], item['description'],
-                item['pic'], '0')
+            sql = """insert into video_info(aid,bvid,title,author,description,cover_img,is_download) values ('%s','%s','%s','%s','%s','%s','%s');""" % (
+                item['aid'], item['bvid'], item['title'], item['author'],
+                item['description'], item['pic'], '0')
             try:
                 cursor.execute(sql)
                 self.db.commit()
-                self.log.debug(f"[bold green]INSERT {item['bvid']} SUCCESSFULLY[/]")
+                self.log.debug(
+                    f"[bold green]INSERT {item['bvid']} SUCCESSFULLY[/]")
             except sqlite3.IntegrityError:
                 self.log.info(f"[bold blue]{item['title']} HAS DOWNLOADED[/]")
                 self.db.rollback()
@@ -87,7 +89,8 @@ class Bili_UP:
             sys.argv = ['you-get', '-o', "./video/" + str(row[1]), video_url]
             you_get.main()
             self.log.info(f"[bold blue]{row[0]} START DOWNLOADING[/]")
-            _sql = """update video_info SET is_download=1 where bvid='%s';""" % (row[2])
+            _sql = """update video_info SET is_download=1 where bvid='%s';""" % (
+                row[2])
             try:
                 cursor.execute(_sql)
                 self.db.commit()
@@ -102,25 +105,14 @@ class Bili_UP:
         cursor = self.db.cursor()
         cursor.execute(sql)
         data = cursor.fetchall()
-        self.log.info(f"[bold green blink]{data[0][0]}/{all_videos} VIDEOS DOWNLOADED[/]")
+        self.log.info(
+            f"[bold green blink]{data[0][0]}/{all_videos} VIDEOS DOWNLOADED[/]"
+        )
 
     def run(self):
         self.get_video_list()
         self.downloader()
-    
+
     def runx(self):
         # TODO 多线程
         pass
-
-
-
-
-
-
-
-
-
-
-
-
-
